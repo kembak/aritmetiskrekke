bd90cb12a6b394d42633cc9af5222827e3aa9051
kembak <kennethmbakken@gmail.com>
2024-09-26 12:16:33 +0200

    sasd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let a1 = 18
+    let k = 12
+    let i = 1
+    let an = 0
+    let n = 0
+ function rekke(n) {
+    if (n>0) {
+        an = a1 + (n-1)*k
+    }
+ }
+ function sum(n) {
+    while (i<=n) {
+        an = a1 + (n-1)*k
+        i = i+1
+    }
+ }
+</script>
+<input type="number" bind:value={n}>
+<button on:click={rekke(n)}>rekke</button>
+<p>{an}</p>
